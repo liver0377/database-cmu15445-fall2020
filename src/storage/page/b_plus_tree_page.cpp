9c8adb3a959777f6c4cfbfb400f478df4018b447
liver0377 <3435951572@qq.com>
2022-01-18 12:35:28 +0800

    删除了b_plus_tree_page的原本实现

--- src/storage/page/b_plus_tree_page.cpp
+++ src/storage/page/b_plus_tree_page.cpp
@@ -17,46 +17,46 @@ namespace bustub {
  * Helper methods to get/set page type
  * Page type enum class is defined in b_plus_tree_page.h
  */
-bool BPlusTreePage::IsLeafPage() const { return page_type_ == IndexPageType::LEAF_PAGE; }
+bool BPlusTreePage::IsLeafPage() const {}
 // 若父节点page id不存在，则为RootPage
-bool BPlusTreePage::IsRootPage() const { return parent_page_id_ == INVALID_PAGE_ID; }
-void BPlusTreePage::SetPageType(IndexPageType page_type) { page_type_ = page_type; }
+bool BPlusTreePage::IsRootPage() const {}
+void BPlusTreePage::SetPageType(IndexPageType page_type) {}
 
 /*
  * Helper methods to get/set size (number of key/value pairs stored in that
  * page)
  */
-int BPlusTreePage::GetSize() const { return size_; }
-void BPlusTreePage::SetSize(int size) { size_ = size; }
-void BPlusTreePage::IncreaseSize(int amount) { size_ += amount; }  // size增加amount
+int BPlusTreePage::GetSize() const {}
+void BPlusTreePage::SetSize(int size) {}
+void BPlusTreePage::IncreaseSize(int amount) {}  // size增加amount
 
 /*
  * Helper methods to get/set max size (capacity) of the page
  */
-int BPlusTreePage::GetMaxSize() const { return max_size_; }  // 叶页面和内部页面的max_size不同（此处不用体现）
-void BPlusTreePage::SetMaxSize(int size) { max_size_ = size; }
+int BPlusTreePage::GetMaxSize() const {}  // 叶页面和内部页面的max_size不同（此处不用体现）
+void BPlusTreePage::SetMaxSize(int size) {}
 
 /*
  * Helper method to get min page size
  * Generally, min page size == max page size / 2
  */
-int BPlusTreePage::GetMinSize() const { return max_size_ / 2; }  // todo 此处要修改？
+int BPlusTreePage::GetMinSize() const {}  // todo 此处要修改？
 
 /*
  * Helper methods to get/set parent page id
  */
-page_id_t BPlusTreePage::GetParentPageId() const { return parent_page_id_; }
-void BPlusTreePage::SetParentPageId(page_id_t parent_page_id) { parent_page_id_ = parent_page_id; }
+page_id_t BPlusTreePage::GetParentPageId() const {}
+void BPlusTreePage::SetParentPageId(page_id_t parent_page_id) {}
 
 /*
  * Helper methods to get/set self page id
  */
-page_id_t BPlusTreePage::GetPageId() const { return page_id_; }
-void BPlusTreePage::SetPageId(page_id_t page_id) { page_id_ = page_id; }
+page_id_t BPlusTreePage::GetPageId() const {}
+void BPlusTreePage::SetPageId(page_id_t page_id) {}
 
 /*
  * Helper methods to set lsn
  */
-void BPlusTreePage::SetLSN(lsn_t lsn) { lsn_ = lsn; }
+void BPlusTreePage::SetLSN(lsn_t lsn) {}
 
 }  // namespace bustub
